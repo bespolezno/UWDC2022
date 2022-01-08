42df4e8e5564ede409b6cbb278c0ba88d72cd80f
Ivan Goncharov <doctorlalk4@gmail.com>
2022-01-08 17:06:03 +0500

    Added category, tag and type index

---
+++ app/Http/Controllers/TypeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        return response()->json(Type::all()->map->name);
+    }
+}
