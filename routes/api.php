5bb7d7cd120ba58ae0a69aea2a08f9a601d04933
Ivan Goncharov <doctorlalk4@gmail.com>
2022-01-08 15:24:53 +0500

    Added routes for api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/types', [\App\Http\Controllers\TypeController::class, 'index']);
+Route::get('/categories', [\App\Http\Controllers\CategoryController::class, 'index']);
+Route::get('/tags', [\App\Http\Controllers\TagController::class, 'index']);
+
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', [\App\Http\Controllers\AuthController::class, 'user']);
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+
+    Route::apiResource('/sessions', \App\Http\Controllers\SessionController::class)->except(['destroy']);
 });
